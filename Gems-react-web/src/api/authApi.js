04e2ab4a4272b1c809ba39cf33f65fef8de4bef4
Sushmin <gems7.divine@gmail.com>
2026-07-23 13:04:26 +0530

    done

--- Gems-react-web/src/api/authApi.js
+++ Gems-react-web/src/api/authApi.js
@@ -1,5 +1,5 @@
 const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:8080/api";
+  import.meta.env.VITE_API_BASE_URL || "http://localhost:8081/api";
 
 export const loginUser = async (email, password) => {
   try {
