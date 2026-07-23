492472e9689d6cec3ff04a7132210e895a3a42ea
Sushmin <gems7.divine@gmail.com>
2026-07-24 01:03:43 +0530

    done

--- Gems-react-web/vite.config.js
+++ Gems-react-web/vite.config.js
@@ -13,21 +13,15 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
 
   server: {
     host: "0.0.0.0",
     port: 5173,
-
-    proxy: {
-      "/api": {
-        target: "http://backend:8080",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+    allowedHosts: [
+      "82.25.104.27",
+      "frontend",
+      "localhost"
+    ]
+  }
 });
