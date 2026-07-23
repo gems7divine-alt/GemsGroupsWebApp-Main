06e871df60fe267a2a0b15b2d872b876d742f112
Sushmin <gems7.divine@gmail.com>
2026-07-23 16:59:12 +0530

    done

--- Gems-react-web/src/api/authApi.js
+++ Gems-react-web/src/api/authApi.js
@@ -1,5 +1,4 @@
-const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://82.25.104.27:8080/api";
+const API_BASE_URL = "http://82.25.104.27:8080/api";
 
 export const loginUser = async (email, password) => {
   try {
--- GemsBackend/src/main/java/GemsGroup/GemsBackend/Controller/AttendanceController.java
+++ GemsBackend/src/main/java/GemsGroup/GemsBackend/Controller/AttendanceController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/attendance")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 public class AttendanceController {
 
     @Autowired
